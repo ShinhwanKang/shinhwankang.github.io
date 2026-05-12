58562c858ab2ba6079777f6219e07e53eef17a9b
ShinhwanKang <60728513+ShinhwanKang@users.noreply.github.com>
2026-05-12 11:22:28 +0000

    Deploying to gh-pages from @ ShinhwanKang/shinhwankang.github.io@a363ccc17eb278a617008f384f07a9be6ccc3453 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "post-a-post-with-plotly-js",
+  },{id: "nav-download-cv",
+          title: "Download CV",
+          description: "Download the above CV :)",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
