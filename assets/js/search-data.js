3d144fb1fedcfe963edd92cdcb7bf8fc00aa5e5b
ShinhwanKang <60728513+ShinhwanKang@users.noreply.github.com>
2026-05-12 11:26:49 +0000

    Deploying to gh-pages from @ ShinhwanKang/shinhwankang.github.io@a6b19f7a753eb982e69026b6d2b700e8479b0c65 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "publications by categories in reversed chronological order.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
